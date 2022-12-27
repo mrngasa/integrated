e86b7d795491bf4b4ad012ca98f8db939c8d6e4c
mr ngasa <mrngasa96@gmail.com>
2022-12-27 23:17:18 +0630

    "server url for render"

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:8800'
+      '/api':'https://integrated-api.onrender.com'
     }
   },
   plugins:[react()]
